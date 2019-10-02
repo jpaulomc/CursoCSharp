bade729e979d39ed24017797b9760e641d3cd805
João Paulo de Moraes da Costa <joao.paulo@univag.edu.br>
2019-10-02 19:48:51 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CursoCSharpTDS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
